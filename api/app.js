823485e470963e294ca466ab49c021d612c9437c
aditya dhanraj <adityadhanraj576@gmail.com>
2025-08-23 11:12:38 +0530

    set view engine

--- api/app.js
+++ api/app.js
@@ -27,10 +27,9 @@ app.use(express.static('public'));
 // app.use('/./public', express.static(path.join(__dirname, 'public')));
 
 //view engine
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
-
 app.use(express.static(path.join(__dirname, '..', 'public')));
-
 app.use('/public', express.static(path.join(__dirname, '..', 'public')));
 
 // Database connection
